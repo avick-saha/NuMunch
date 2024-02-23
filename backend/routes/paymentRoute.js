8265dbe57066b1921e90dce6fae19b2e323d9e25
Avick Saha <70565523+avick-saha@users.noreply.github.com>
2024-02-23 22:36:53 +0530

    Update paymentRoute.js

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const {
   processPayment,
-  // sendStripeApiKey,
-  validatePayment
+  validatePayment,
+  processPaymentRazorpay,
+  validatePaymentRazorpay
 } = require("../controllers/paymentController");
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
 
 // router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 router.route("/create-checkout-session").post(processPayment);
-// router.route("/payment/process/validate").post(isAuthenticatedUser, validatePayment);
-// router.route("/payment/process/validate").post(validatePayment);
 
-// router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
+
+router.route("/process-payment-razorpay").post(processPaymentRazorpay);
+router.route("/validate-payment-razorpay").post(validatePaymentRazorpay);
 
 module.exports = router;
